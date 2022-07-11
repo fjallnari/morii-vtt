24a471552cff299732908cba1fba298bc8e2a85d
Ashmogh <draconirx@gmail.com>
2022-07-11 19:45:36 +0200

    Add character limit for InPlaceEditBox

--- src/client/src/components/InPlaceEditBox.svelte
+++ src/client/src/components/InPlaceEditBox.svelte
@@ -4,6 +4,7 @@
 
     export let value: string;
     export let boxLabel: string;
+    export let characterLimit: number = 0;
     export let inlineStyle: string = '';
     export let editWidth: string = '';
     export let editHeight: string = '';
@@ -13,7 +14,12 @@
 
 <box class="box-with-label" style={inlineStyle}>
     <div class="box-main-text">
-        <InPlaceEdit bind:value={value} editWidth={editWidth} editHeight={editHeight} on:submit={onSubmitFce}/>
+        <InPlaceEdit 
+            bind:value={value} 
+            editWidth={editWidth} 
+            editHeight={editHeight} 
+            on:submit={() => { characterLimit !== 0 && value.length > characterLimit ? value = value.slice(0, characterLimit) : value; onSubmitFce()}}
+        />
     </div>
     <div class="box-justify-filler"></div>
     <div class="box-label">

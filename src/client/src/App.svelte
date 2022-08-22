f14eb4b1599377a34603ac98d7c09eaf13780bc7
Ashmogh <draconirx@gmail.com>
2022-08-22 15:24:32 +0200

    Add wizard qcreate data

--- src/client/src/App.svelte
+++ src/client/src/App.svelte
@@ -277,7 +277,7 @@
 		--sv-border: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%) !important;
 		--sv-item-color: var(--clr-text) !important;
 		--sv-item-active-bg: var(--clr-accent-dark) !important;
-		--sv-dropdown-height: 350px !important;
+		--sv-dropdown-height: 375px !important;
 	}
 
 	:global(.mdc-dialog .mdc-dialog__surface) {

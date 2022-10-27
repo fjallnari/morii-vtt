bea9f59c3697c2b6a11cc1f255882d969cf98727
Ashmogh <draconirx@gmail.com>
2022-10-27 17:33:21 +0200

    Add drag&drop to initiative

---
+++ src/client/src/components/Game/GameOverview/DeleteInitEntity.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import {dndzone} from 'svelte-dnd-action';
+	const flipDurationMs = 200;
+	
+	export let items = [];
+	
+	function handleSort(e) {
+		items = e.detail.items;
+	}
+
+    const deleteItem = (e) => {
+        items = e.detail.items;
+        items = [];
+    }
+
+</script>
+
+<section use:dndzone={{items, flipDurationMs, dropTargetStyle: {outline: '', 'width': '100%'}}} on:consider={handleSort} on:finalize={deleteItem}>
+    <slot></slot>
+</section>
+
+<style>
+    section {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
--- src/client/src/components/Game/GameOverview/Initiative.svelte
+++ src/client/src/components/Game/GameOverview/Initiative.svelte
@@ -3,9 +3,23 @@
     import { Icon } from '@smui/icon-button';
     import type InitiativeData from "../../../interfaces/InitiativeData";
     import { nanoid } from "nanoid/non-secure";
+    import { dndzone, SOURCES, TRIGGERS	 } from 'svelte-dnd-action';
+	import { flip } from 'svelte/animate';
+    import DeleteInitEntity from "./DeleteInitEntity.svelte";
+    import InPlaceEdit from "../../InPlaceEdit.svelte";
     
     export let gameData: GameData;
     export let initiative: InitiativeData;
+
+    const flipDurationMs = 200;
+	let dragDisabled = true;
+    let deletedEntities = [];
+
+	$: items = initiative.order; // has to be named this way for svelte-dnd-action
+
+    $: if (deletedEntities.length !== 0) {
+        initiative.order = initiative.order.filter(initEntity => initEntity.id !== deletedEntities[0].id);
+    }
 
     const sortInitOrder = () => {
         initiative.order = initiative.order.sort((a, b) => ~~a.value < ~~b.value ? 1 : -1);
@@ -32,34 +46,93 @@
             initiative.order = [... initiative.order.slice(1), initiative.order[0]];
         }
     }
+	
+	const handleConsider = (e) => {
+		const {items: newItems, info: {source, trigger}} = e.detail;
+		items = newItems;
+		// Ensure dragging is stopped on drag finish via keyboard
+		if (source === SOURCES.KEYBOARD && trigger === TRIGGERS.DRAG_STOPPED) {
+			dragDisabled = true;
+		}
+	}
+
+	const handleFinalize = (e) => {
+		const {items: newItems, info: {source}} = e.detail;
+		items = newItems;
+		// Ensure dragging is stopped on drag finish via pointer (mouse, touch)
+		if (source === SOURCES.POINTER) {
+			dragDisabled = true;
+		}
+	}
+
+	const startDrag = (e) => {
+		// preventing default to prevent lag on touch devices (because of the browser checking for screen scrolling)
+		e.preventDefault();
+		dragDisabled = false;
+	}
+
+	const handleKeyDown = (e) => {
+		if ((e.key === "Enter" || e.key === " ") && dragDisabled) dragDisabled = false;
+	}
 
 </script>
 
 
 <box class='initiative-container'>
-    <div class='initiative-list'>
-        {#each initiative.order as initEntity}
-            <box class='init-entity-tag'>
-                {#if initEntity.id === initiative.topID}
-                    <Icon class="material-icons">{'hourglass_top'}</Icon>
-                {/if}
-                {`${initEntity.name} ~ ${initEntity.value}`}
+    <section
+        class='initiative-list'
+        use:dndzone="{{ items, dragDisabled, flipDurationMs, dropTargetStyle: {outline: '', 'box-shadow': '0px 0px 0px 2px var(--clr-accent-dark)', 'border-radius': '4px'} }}"
+        on:consider="{handleConsider}"
+        on:finalize="{handleFinalize}"
+    >
+        {#each items as initEntity (initEntity.id)}
+            <box class='init-entity-tag' animate:flip="{{ duration: flipDurationMs }}">
+                <span class="entity-icon">
+                    {#if initEntity.id === initiative.topID}
+                        <Icon class="material-icons">{'hourglass_top'}</Icon>
+                    {/if}
+                </span>
+                <span class='entity-name'>
+                    <InPlaceEdit bind:value={initEntity.name} editWidth={'8em'} editHeight={'1em'}/>
+                </span>
+
+                <span class="entity-value">
+                    <InPlaceEdit bind:value={initEntity.value} editWidth={'1.75em'} editHeight={'1em'}/>
+                </span>
+                <span class="entity-drag-handle">
+                    <Icon class="material-icons handle"
+                            tabindex={dragDisabled? 0 : -1} 
+                            aria-label="drag-handle"
+                            style={dragDisabled ? 'cursor: grab' : 'cursor: grabbing'}
+                            on:mousedown={startDrag} 
+                            on:touchstart={startDrag}
+                            on:keydown={handleKeyDown}
+                    >{'drag_indicator'}</Icon>
+                </span>
             </box>
         {/each}
-    </div>
-    <box class='initiative-menu'>
-        <sendable class="init-menu-icon" on:click={() => clearInitOrder()}>
-            <Icon class="material-icons">{'restart_alt'}</Icon>
-        </sendable>
-        <sendable class="init-menu-icon" on:click={() => addNewInitEntity()}>
-            <Icon class="material-icons">{'add'}</Icon>
-        </sendable>
-        <sendable class="init-menu-icon" on:click={() => sortInitOrder()}>
-            <Icon class="material-icons">{'sort'}</Icon>
-        </sendable>
-        <sendable class="init-menu-icon" on:click={() => passTurn()}>
-            <Icon class="material-icons">{'forward'}</Icon>
-        </sendable>
+    </section>
+    <box class='initiative-menu{!dragDisabled ? ' delete-mode': ''}'>
+        {#if dragDisabled}
+            <sendable class="init-menu-icon" on:click={() => clearInitOrder()}>
+                <Icon class="material-icons">{'restart_alt'}</Icon>
+            </sendable>
+            <sendable class="init-menu-icon" on:click={() => addNewInitEntity()}>
+                <Icon class="material-icons">{'add'}</Icon>
+            </sendable>
+            <sendable class="init-menu-icon" on:click={() => sortInitOrder()}>
+                <Icon class="material-icons">{'sort'}</Icon>
+            </sendable>
+            <sendable class="init-menu-icon" on:click={() => passTurn()}>
+                <Icon class="material-icons">{'forward'}</Icon>
+            </sendable>
+        {:else}
+            <DeleteInitEntity bind:items={deletedEntities}>
+                <div class="delete-item init-menu-icon">
+                    <Icon class="material-icons">{'delete_outline'}</Icon>
+                </div>
+            </DeleteInitEntity>
+        {/if}
     </box>
 </box>
 
@@ -77,17 +150,24 @@
         justify-content: flex-start;
         align-items: center;
         gap: 0.5em;
-        margin-top: 1em;
+        margin: 1em 0.5em;
         height: 85%;
         overflow-y: auto;
         scrollbar-width: thin;
     }
 
     .init-entity-tag {
-        display: flex;
+        /* display: flex;
         justify-content: center;
-        align-items: center;
-        gap: 0.4em;
+        align-items: center; */
+
+        display: grid; 
+        grid-template-columns: 1fr 6fr 2fr 1fr; 
+        grid-template-rows: 1fr; 
+        gap: 0.4em; 
+        grid-template-areas: 
+            "entity-icon entity-name entity-value entity-drag-handle";
+        
         width: 90%;
         background-color: var(--clr-box-bg-light);
         font-family: Quicksand;
@@ -99,6 +179,21 @@
         background: linear-gradient(135deg, var(--clr-accent-normal) 20%, var(--clr-box-bg-light) 20%);
     }
 
+    .init-entity-tag span {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .entity-name { grid-area: entity-name; }
+
+    .entity-value { grid-area: entity-value;
+        font-weight: var(--semi-bold);
+    }
+
+    .entity-icon { grid-area: entity-icon; }
+    .entity-drag-handle { grid-area: entity-drag-handle; }
+
     .initiative-menu {
         position: absolute;
         left: 50%;
@@ -106,11 +201,21 @@
         width: 100%;
         background-color: var(--clr-box-bg-light);
         height: 10%;
+        min-height: 2em;
         top: 90%;
         display: flex;
         justify-content: center;
         align-items: center;
         gap: 0.75em;
+    }
+
+    .initiative-menu.delete-mode {
+        background-color: var(--clr-contrast-normal);
+    }
+
+    .delete-item {
+        position: relative;
+        z-index: 2;
     }
 
     .init-menu-icon {

227e0a39eefb7464e40c2446f7fde3c2eb520038
ashmogh <draconirx@gmail.com>
2023-02-15 02:31:30 +0100

    Extend cairn generate script to split stats

--- src/scripts/cairn/monsters_to_json.py
+++ src/scripts/cairn/monsters_to_json.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 try: 
     from nanoid import generate
@@ -9,7 +10,17 @@ except ModuleNotFoundError:
 
 INPUT_DIR = 'cairn_monsters'
 OUTPUT_PATH = 'monsters.json'
+COMPLEX_STATS_RE = r"(?P<hp>[0-9]+) HP,(?: (?P<armor>[0-9]+) Armor,)*(?: (?P<str>[0-9]+) STR,)*(?: (?P<dex>[0-9]+) DEX,)*(?: (?P<wil>[0-9]+) WIL(?:,)?)*(?: )?(?P<attacks>(?:[a-zA-Z -]*(?: )?\([^\(\)]+\)(?:,| or)*)*)(?P<special>.*)"
 
+
+# ! fixes in original Cairn SRD needed 
+# Great White Shark: replace . (dot) with ,
+# Warper: has WILL instead of WIL
+# Giant Scorpion: has MIND instead of WIL
+# Cat, Sabre-Toothed Tiger: flipped STR tag and actual value
+# MINOR ISSUES
+# Fire Beetle, Catoplebas: missing space between attack name and dmg
+# Green Dragon, Blue Dragon, Giant Octopus, Treant, Warper, Flame Lizard, Griffon, Bone Construct, Hydra, Fire Elemental, Wood Troll, Roc: missing italics for blast
 
 def load_source(path):
     source_file = open(path, "r")
@@ -24,20 +35,33 @@ def dump_result(path, obj):
     json.dump(obj, output_file, indent=2)
     output_file.close()
 
+def fix_missing_parenthesis(attack: str):
+    return attack + ")" if attack[-1] != ")" else attack
 
-def convert_monster_to_json(lines):
+def convert_monster_to_json(lines, complex):
     monster = lines[6:]
     stats = monster[2].strip()
+    stats_complex = {}
     description = ''.join(monster[4:]).strip()
     
-    return {"name": monster[0].replace('#', '').strip(), "stats": stats, "description": description, "id": generate(size=10)}
+    # split HP, Armor, STR, DEX, WIL, attacks and special into separate properties
+    
+    if complex:
+        match = re.match(COMPLEX_STATS_RE, stats)
+        stats_complex = match.groupdict() if match else stats_complex
+        
+        stats_complex['attacks'] = list(filter(None, re.split('\), |\),| or | or|, or ', stats_complex['attacks']))) if stats_complex['attacks'] else []
+        stats_complex['attacks'] = list(map(fix_missing_parenthesis, stats_complex['attacks']))
+        stats_complex = {k: v for k, v in stats_complex.items() if v}
+    
+    return {"name": monster[0].replace('#', '').strip(), "stats": stats, **stats_complex, "description": description, "id": generate(size=10)}
 
-def get_monsters_as_arr(directory = INPUT_DIR):
+def get_monsters_as_json(directory = INPUT_DIR, complex = False):
     print(f'Generating Cairn monsters > {OUTPUT_PATH}')
     monsters_array = []
     for filename in os.scandir(directory):
         if filename.is_file() and filename.name.endswith('.md'):
-            monsters_array.append(convert_monster_to_json(load_source(filename.path)))
+            monsters_array.append(convert_monster_to_json(load_source(filename.path), complex))
             
     dump_result(OUTPUT_PATH, monsters_array)
     
@@ -46,4 +70,4 @@ def get_monsters_as_arr(directory = INPUT_DIR):
 
 
 if __name__ == "__main__":
-    get_monsters_as_arr()
+    get_monsters_as_json(complex=True)

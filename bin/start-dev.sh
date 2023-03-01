774e6519afcd8bfc37e820222c8874c377f16276
ashmogh <draconirx@gmail.com>
2023-03-01 13:31:54 +0100

    Bump node version to 18

--- bin/start-dev.sh
+++ bin/start-dev.sh
@@ -3,10 +3,10 @@
 function create_volumes(){
     MY_UID=$1
     MY_GID=$2
-    docker volume create node-16
+    docker volume create node-18
     docker volume create morii-mongo
     docker volume create morii-client
-    docker run --rm -it -v node-16:/node-16 busybox su -s /bin/sh -c "chown $MY_UID:$MY_GID /node-16"
+    docker run --rm -it -v node-18:/node-18 busybox su -s /bin/sh -c "chown $MY_UID:$MY_GID /node-18"
     docker run --rm -it -v morii-mongo:/morii-mongo busybox su -s /bin/sh -c "chown $MY_UID:$MY_GID /morii-mongo"
     docker run --rm -it -v morii-client:/morii-client busybox su -s /bin/sh -c "chown $MY_UID:$MY_GID /morii-client"
 }
@@ -18,7 +18,7 @@ function start_docker_compose(){
 
     MY_UID="${MY_UID}" MY_GID="${MY_GID}" docker-compose down --remove-orphans
     echo "Starting docker-compose up"
-    docker image pull node:16
+    docker image pull node:18
     MY_UID="${MY_UID}" MY_GID="${MY_GID}" docker-compose -f $DOCKER_COMPOSE_FILE up -d --remove-orphans $SERVICES
     MY_UID="${MY_UID}" MY_GID="${MY_GID}" docker-compose -f $DOCKER_COMPOSE_FILE logs --follow
 }
